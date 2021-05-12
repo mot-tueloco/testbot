d2e3e900554109bf00b45407dea9bf475adf017d
mot-tueloco <guim5249548@163.com>
2021-05-12 18:14:14 +0800

    initial

---
+++ testbot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testbot
+//
+//  Created by Bob Chen on 11/8/15.
+//  Copyright © 2015 vcools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testbot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testbot
+//
+//  Created by Bob Chen on 11/8/15.
+//  Copyright © 2015 vcools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
